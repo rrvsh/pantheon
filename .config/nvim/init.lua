979655751b68b67a323a1c1a79508e11586ffa94
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-02-20 06:21:48 +0800

    feat(neovim): add nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,16 @@
+vim.opt.number = true -- Show line numbers
+vim.opt.relativenumber = true -- Show relative line numbers
+
+vim.opt.cursorline = true -- Highlight current line
+
+-- Indentation
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = false
+
+-- Visual tweaks
+vim.opt.termguicolors = true -- Enable true color support in the terminal
+vim.opt.background = "dark" -- Or "light" depending on your preference
+vim.opt.signcolumn = "yes" -- Always show sign column (for diagnostics, etc.)
