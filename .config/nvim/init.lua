a7b8a29d6b550c49ad64f95318ecdb9ddb66ac64
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-02-20 18:40:49 +0800

    feat(nvim): add lazy.nvim (WIP)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- Plugins
+require("config.lazy")
+
+-- Visual
 vim.opt.number = true -- Show line numbers
 vim.opt.relativenumber = true -- Show relative line numbers
 
---
+++ .config/nvim/lua/config/lazy.lua
@@ -0,0 +1,4 @@
+-- Bootstrap lazy.nvim
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- Get the path for the vim install and append /lazy/lazy.nvim to it
