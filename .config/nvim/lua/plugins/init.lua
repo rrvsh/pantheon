ef3a8a1b6ab16309d9405bc9ddcc9d5fded12fc6
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-02-21 05:10:41 +0800

    feat(nvim): add catppuccin theme

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,16 +1,27 @@
 return { -- Lua table
+  -- nvim.tree.lua
   "nvim-tree/nvim-tree.lua", -- github repo
   version = "*", -- latest version
   lazy = false, -- dont use lazy loading
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  config = function()
-    require("nvim-tree").setup {
-      hijack_cursor = true, -- makes cursor stick to first char
-      view = {
-        side = "right",
-      },
-    }
-  end,
+  opts = {
+    hijack_cursor = true, -- makes cursor stick to first char
+    view = {
+      side = "right",
+    }, 
+  },
+
+  -- catppuccin
+  { 
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    version = "*",
+    lazy = false,
+    config = function()
+      vim.cmd.colorscheme = "catppuccin"
+    end,
+  },
 }
