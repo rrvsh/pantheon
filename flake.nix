ef89e56b6e3318e5f317c8b51a7991dffe457df7
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-02-22 12:38:34 +0800

    build(nixos): add home-manager

--- flake.nix
+++ flake.nix
@@ -1,15 +1,23 @@
 {
-  description = "simple NixOS flake";
+  description = "simple NixOS flake with home-manager";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.nemesis = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
+
+ 	home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+          home-manager.users.rafiq = import ./home.nix;
+        }
       ];
     };
   };
---
+++ home.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "rafiq";
+  home.homeDirectory = "/home/rafiq";
+
+  # home.file.".config/" = {
+  #   source = ./.config;
+  #   recursive = true;
+  # };
+
+  home.packages = with pkgs; [
+    fastfetch
+    neovim
+    zip
+    unzip
+    ripgrep
+  ];
+  
+  programs.git = {
+    enable = true;
+    userName = "Mohammad Rafiq";
+    userEmail = "mohammadrafiq567@gmail.com";
+  };
+
+  #TODO add neovim option
+
+  #TODO add starship
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    shellAliases = {
+      ll = "ls -la";
+    };
+  };
+  
+  home.stateVersion = "24.11";
+  programs.home-manager.enable = true;
+}
+
+  
