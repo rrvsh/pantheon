8755bbf84609720a86e2e37483553c79314dcd8f
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-02-22 10:56:57 +0800

    feat: add ssh to config.nix

--- configuration.nix
+++ configuration.nix
@@ -78,7 +78,9 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
