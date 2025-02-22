fe5b8d1320b4cf9f40f98fc6041219c2770bb8b2
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-02-22 11:06:52 +0800

    feat: add flakes support to configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -61,11 +61,13 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
   ];
+  environment.variables.EDITOR = "vim";
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
