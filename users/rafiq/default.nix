0bcf8a90edf7bf067463836cd217c9e867b65f3b
rafiq <mohammadrafiq567@gmail.com>
2025-02-27 06:37:32 +0800

    feat(hyprland): add initial configuration options

--- users/rafiq/default.nix
+++ users/rafiq/default.nix
@@ -18,6 +18,32 @@
     };
   };
 
+  wayland.windowManager.hyprland = {
+    enable = true;
+    systemd.enable = false; # Required for UWSM compat
+    systemd.variables = ["--all"];
+    # Use tha packages defined in the system configuration
+    package = null;
+    portalPackage = null;
+    settings = {
+      monitor = [
+        "HDMI-A-2, 3840x2160@60, 0x0, 2"
+        "DP-4, 1920x1080@60, -1920x0, 1"
+        ", preferred, auto, 1"
+      ];
+      "$terminal" = "kitty";
+      "$browser" = "firefox";
+      "$mainMod" = "SUPER";
+      bind = [
+        "$mainMod, Q, exec, uwsm app -- $terminal"
+        "$mainMod, W, killactive"
+        "$mainMod, E, exec, uwsm app -- $browser"
+        "$mainMod, M, exec, uwsm stop"
+      ];
+    };
+  };
+
+
   home.stateVersion = "25.05";
   programs.home-manager.enable = true;
 }
