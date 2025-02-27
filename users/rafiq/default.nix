e727f258047271c9200c0344ebb43c4fe0288110
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-02-28 05:13:20 +0800

    refactor(hyprland): move hyprland into its own module

--- users/rafiq/default.nix
+++ users/rafiq/default.nix
@@ -5,6 +5,7 @@
     ../../modules/home-git.nix
     ../../modules/home-tmux.nix
     ../../modules/home-sh.nix
+    ../../modules/home-wm.nix
   ];
 
   home = {
@@ -26,32 +27,6 @@
     };
 
     home-manager.enable = true;
-  };
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-    systemd.enable = false; # Required for UWSM compat
-    systemd.variables = ["--all"];
-    # Use tha packages defined in the system configuration
-    package = null;
-    portalPackage = null;
-    settings = {
-      monitor = [
-        "HDMI-A-2, 3840x2160@60, 0x0, 2"
-        "DP-4, 1920x1080@60, -1920x0, 1"
-        ", preferred, auto, 1"
-      ];
-      "$terminal" = "kitty";
-      "$browser" = "firefox";
-      "$mainMod" = "SUPER";
-      bind = [
-        "$mainMod, Q, exec, uwsm app -- $terminal"
-        "$mainMod, W, killactive"
-        "$mainMod, E, exec, uwsm app -- $browser"
-        "$mainMod, M, exec, uwsm stop"
-      ];
-      debug.disable_logs = false;
-    };
   };
 
   services = {
