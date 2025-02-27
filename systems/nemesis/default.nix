66f3c5d32b14e6b7630460a09c89533daf8960d9
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-02-28 07:02:16 +0800

    refactor: moved common system options to their own file

--- systems/nemesis/default.nix
+++ systems/nemesis/default.nix
@@ -7,6 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
+      ../../modules/common.nix
       ./hardware-configuration.nix
       ../../scripts/hyprland-tty-launch.nix
       ../../modules/nvidia.nix # Graphics settings for Nvidia GPUs
@@ -22,43 +23,6 @@
       fsType = "ntfs-3g";
       options = [ "rw" "uid=rafiq" ];
     };
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Set your time zone.
-  time.timeZone = "Asia/Singapore";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_SG.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_SG.UTF-8";
-    LC_IDENTIFICATION = "en_SG.UTF-8";
-    LC_MEASUREMENT = "en_SG.UTF-8";
-    LC_MONETARY = "en_SG.UTF-8";
-    LC_NAME = "en_SG.UTF-8";
-    LC_NUMERIC = "en_SG.UTF-8";
-    LC_PAPER = "en_SG.UTF-8";
-    LC_TELEPHONE = "en_SG.UTF-8";
-    LC_TIME = "en_SG.UTF-8";
-  };
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.rafiq = {
-    isNormalUser = true;
-    description = "rafiq";
-    extraGroups = [ "networkmanager" "wheel" ];
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
