1f8c24f0a873e3f05ed1fafdbd0ec60ba6618ef2
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-02-28 19:24:21 +0800

    rebase: add networking module back to system config

--- systems/nemesis.nix
+++ systems/nemesis.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hw-nemesis.nix
-#      ../modules/networking.nix
+      ../modules/networking.nix
     ];
 
   # Bootloader.
@@ -87,7 +87,7 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
