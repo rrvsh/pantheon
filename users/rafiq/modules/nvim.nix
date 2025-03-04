f51b21889c4283d01060041ccf1b18f1c2e91285
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-05 07:08:42 +0800

    refactor: move nvim config to outofstoresymlink

--- users/rafiq/modules/nvim.nix
+++ users/rafiq/modules/nvim.nix
@@ -1,10 +1,12 @@
-{ lib, ... }: {
+{ config, lib, ... }: let
+  nvimPath = "${config.home.homeDirectory}/repos/dotfiles/users/rafiq/.config/nvim";
+in {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
-    extraLuaConfig = lib.fileContents ../.config/nvim/init.lua;
   };
+  xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink nvimPath;
 }
