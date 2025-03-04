e6214925fcc73ac8f0850a411b6e4b580a10a4fa
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-05 06:14:29 +0800

    refactor: move system locale options to their own file

--- systems/nemesis.nix
+++ systems/nemesis.nix
@@ -6,6 +6,7 @@
     # Include the results of the hardware scan.
     ./hw-nemesis.nix
     ./modules/networking.nix
+    ./modules/locale.nix
   ];
 
   # Bootloader.
@@ -13,24 +14,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nemesis"; # Define your hostname.
-
-  # Set your time zone.
-  time.timeZone = "Asia/Singapore";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_SG.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_SG.UTF-8";
-    LC_IDENTIFICATION = "en_SG.UTF-8";
-    LC_MEASUREMENT = "en_SG.UTF-8";
-    LC_MONETARY = "en_SG.UTF-8";
-    LC_NAME = "en_SG.UTF-8";
-    LC_NUMERIC = "en_SG.UTF-8";
-    LC_PAPER = "en_SG.UTF-8";
-    LC_TELEPHONE = "en_SG.UTF-8";
-    LC_TIME = "en_SG.UTF-8";
-  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.rafiq = {
