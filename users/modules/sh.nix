a0159c20c0c3a2945eb2607d7ef38ce995c023fa
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-04 13:16:56 +0800

    feat(home-manager): add direnv-nix to rafiq config

--- users/modules/sh.nix
+++ users/modules/sh.nix
@@ -7,4 +7,12 @@
       ai = "aichat -r %shell% -e";
     };
   };
+
+  # direnv lets us declare a .envrc in each project directory
+  # and updates the shell with the packages specified.
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    nix-direnv.enable = true;
+  };
 }
