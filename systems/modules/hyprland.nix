e6c63ba73f75b9c64f591b86858e38b2f0e89efe
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-05 08:53:38 +0800

    build(nemesis): add dunst

--- systems/modules/hyprland.nix
+++ systems/modules/hyprland.nix
@@ -8,4 +8,7 @@
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
+  environment.systemPackages = with pkgs; [
+    dunst
+  ];
 }
