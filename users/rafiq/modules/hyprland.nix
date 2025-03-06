1307c5ee4562f137e00da02698dfcd782afd0836
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-06 13:33:53 +0800

    chore: removed old unused lines of code

--- users/rafiq/modules/hyprland.nix
+++ users/rafiq/modules/hyprland.nix
@@ -68,6 +68,4 @@
       ];
     };
   };
-  # xdg.configFile."hypr".source = config.lib.file.mkOutOfStoreSymlink  "${config.home.homeDirectory}/repos/dotfiles/users/rafiq/.config/hypr";
-  # xdg.configFile."hypr/hyprland.conf".enable = false; # Needed so home-manager won't create the config file
 }
