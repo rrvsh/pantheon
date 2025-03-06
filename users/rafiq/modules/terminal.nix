131d214705dfc3c5b14676a1009cee1ecad4ebee
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-06 13:52:47 +0800

    feat(kitty): add kitty keybinds for resizing text

--- users/rafiq/modules/terminal.nix
+++ users/rafiq/modules/terminal.nix
@@ -1,7 +1,14 @@
-{ pkgs, ... }:
-
 {
-  home.packages = with pkgs; [
-    kitty # default terminal emulator for hyprland
-  ];
+  programs.kitty = {
+    enable = true;
+    # font = {
+    #   name = "";
+    #   package = ?;
+    #   size = 32;
+    # };
+    keybindings = {
+      "ctrl+equal" = "change_font_size current +2.0";
+      "ctrl+minus" = "change_font_size current -2.0";
+    };
+  };
 }
