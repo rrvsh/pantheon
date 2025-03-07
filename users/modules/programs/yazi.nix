098b1dbcad8ff740558f650a6434ce75300a9ccf
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-07 17:40:20 +0800

    feat(yazi): add catppuccin theme

--- users/modules/programs/yazi.nix
+++ users/modules/programs/yazi.nix
@@ -3,5 +3,7 @@
     enable = true;
     package = inputs.yazi.packages.${pkgs.system}.default;
     enableBashIntegration = true;
+    settings = {};
+    theme = builtins.fromTOML (builtins.readFile ./yazi/theme.toml);
   };
 }
