24063cd365c09377be98d1e025f6d431e58e61c1
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-11 12:11:08 +0800

    feat: add padding to kitty

--- users/modules/programs/kitty.nix
+++ users/modules/programs/kitty.nix
@@ -5,5 +5,8 @@
       "ctrl+equal" = "change_font_size current +2.0";
       "ctrl+minus" = "change_font_size current -2.0";
     };
+    settings = {
+      window_padding_width = 10;
+    };
   };
 }
