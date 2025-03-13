ab4113b74d7644f9fa5881890af312fa127f548a
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-13 19:10:47 +0800

    feat: add nvf cache

--- systems/modules/networking.nix
+++ systems/modules/networking.nix
@@ -27,11 +27,13 @@
       "https://hyprland.cachix.org"
       "https://cuda-maintainers.cachix.org"
       "https://nix-community.cachix.org"
+      "https://nvf.cachix.org"
     ];
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nvf.cachix.org-1:GMQWiUhZ6ux9D5CvFFMwnc2nFrUHTeGaXRlVBXo+naI="
     ];
   };
 
