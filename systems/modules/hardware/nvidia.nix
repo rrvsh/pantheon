34ba55738827600e3e1b17aa28390541cecb4b68
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-14 01:41:52 +0800

    chore: minor nvidia settings

--- systems/modules/hardware/nvidia.nix
+++ systems/modules/hardware/nvidia.nix
@@ -1,13 +1,24 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   services.xserver.videoDrivers = ["nvidia"];
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      # TODO:
-      # opencl
-      nvidia-vaapi-driver # hardware acceleration
-    ];
+  hardware = {
+    graphics = {
+      enable = true;
+      extraPackages = with pkgs; [
+        # TODO:
+        # opencl
+        nvidia-vaapi-driver # hardware acceleration
+      ];
+    };
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = true;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
+    };
   };
-  hardware.nvidia.powerManagement.enable = true;
-  hardware.nvidia.open = false;
 }
