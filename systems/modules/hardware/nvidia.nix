73ad96f88ea05308d7f6ad03e6782fb95c0954d1
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-14 04:01:54 +0800

    feat: add kernel params for nvidia

--- systems/modules/hardware/nvidia.nix
+++ systems/modules/hardware/nvidia.nix
@@ -3,13 +3,24 @@
   config,
   ...
 }: {
+  # Should not be needed because we set modesetting.enable = true;
+  boot.kernelParams = [
+    "nvidia-drm.modeset=1"
+    "nvidia-drm.fbdev=1"
+  ];
+  nixpkgs.config.nvidia.acceptLicense = true;
   services.xserver.videoDrivers = ["nvidia"];
+  environment.variables = {
+    GBM_BACKEND = "nvidia-drm";
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  };
   hardware = {
+    enableRedistributableFirmware = true;
+    nvidia-container-toolkit.enable = true;
     graphics = {
       enable = true;
       extraPackages = with pkgs; [
-        # TODO:
-        # opencl
         nvidia-vaapi-driver # hardware acceleration
       ];
     };
