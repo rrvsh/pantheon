af005dce39dedc7c14914957211d82d3aa06bc8e
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-14 21:44:19 +0800

    feat: add editorconfig to home-manager

--- users/modules/sh.nix
+++ users/modules/sh.nix
@@ -13,10 +13,20 @@
     ./programs/zsh.nix
     ./scripts
   ];
-
   home.shell.enableShellIntegration = true;
   home.shellAliases = {
     gs = "git status";
     ai = "aichat -r %shell% -e";
+  };
+  editorconfig = {
+    enable = true;
+    settings = {
+      "*" = {
+        indent_style = "space";
+        indent_size = 2;
+        trim_trailing_whitespace = true;
+        insert_final_newline = false;
+      };
+    };
   };
 }
