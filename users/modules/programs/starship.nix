e3070bf04b77f87cb53facdf1c5b76d4032e5d68
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-14 13:55:44 +0800

    feat: change starship prompt

--- users/modules/programs/starship.nix
+++ users/modules/programs/starship.nix
@@ -6,6 +6,10 @@
       add_newline = false;
       format = "$character";
       right_format = "$all";
+      character = {
+        success_symbol = "[\\$](bold green)";
+        error_symbol = "[\\$](bold red)";
+      };
     };
   };
 }
