fc53bafcb90ba0039a1e3ef93d218a09e8a329a3
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-17 16:46:50 +0800

    feat(git): add signing with public key

--- users/modules/programs/git.nix
+++ users/modules/programs/git.nix
@@ -1,15 +1,18 @@
-{
+{config, ...}: {
   home.sessionVariables.GIT_CONFIG_GLOBAL = "$HOME/.config/git/config";
 
   programs.git = {
     enable = true;
     userName = "Mohammad Rafiq";
     userEmail = "mohammadrafiq567@gmail.com";
+    signing.key = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+    signing.signByDefault = true;
     extraConfig = {
       init.defaultBranch = "prime";
       push.autoSetupRemote = true;
       pull.rebase = false;
       core.editor = "nvim";
+      gpg.format = "ssh";
     };
   };
 }
