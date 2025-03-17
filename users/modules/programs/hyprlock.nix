88364f0be0590a34113b3ceeca7c83ca82048093
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-18 01:07:43 +0800

    chore: test hyprlock config change

--- users/modules/programs/hyprlock.nix
+++ users/modules/programs/hyprlock.nix
@@ -2,5 +2,13 @@
   programs.hyprlock = {
     enable = true;
     package = null;
+
+    settings = {
+      general.hide_cursor = true;
+
+      input-field = {
+        fade_on_empty = false;
+      };
+    };
   };
 }
