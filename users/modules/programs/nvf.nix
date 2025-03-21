1312e21291c4a89eff9271a1c78ccaf94d704453
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-22 01:37:37 +0800

    feat(nvf): make ctrl BS delete whole word

--- users/modules/programs/nvf.nix
+++ users/modules/programs/nvf.nix
@@ -42,9 +42,17 @@
       };
       keymaps = [
         {
+          desc = "Open the file path under the cursor, making the file if it doesn't exist.";
           key = "gf";
           mode = "n";
           action = ":cd %:p:h<CR>:e <cfile><CR>";
+          silent = true;
+        }
+        {
+          desc = "Delete the previous word.";
+          key = "<C-BS>";
+          mode = "i";
+          action = "<C-W>";
           silent = true;
         }
       ];
