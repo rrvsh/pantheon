c44174bbc607ff179922bbae1e1cffa9e7fa3fe7
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-22 01:22:28 +0800

    build(yazi): add yazi binary cache

--- systems/modules/networking.nix
+++ systems/modules/networking.nix
@@ -17,7 +17,7 @@
       allowedTCPPorts = [
         22 # SSH
       ];
-      allowedUDPPorts = [];
+      allowedUDPPorts = [ ];
     };
   };
 
@@ -28,12 +28,14 @@
       "https://cuda-maintainers.cachix.org"
       "https://nix-community.cachix.org"
       "https://nvf.cachix.org"
+      "https://yazi.cachix.org"
     ];
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nvf.cachix.org-1:GMQWiUhZ6ux9D5CvFFMwnc2nFrUHTeGaXRlVBXo+naI="
+      "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
     ];
   };
 
