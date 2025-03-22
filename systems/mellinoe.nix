727a8aad625b82350339723e8fd4caa02917b2f4
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-22 17:26:58 +0800

    fix(mellinoe): actually include disko in the config

--- systems/mellinoe.nix
+++ systems/mellinoe.nix
@@ -5,6 +5,7 @@
     ./modules/desktop.nix
     ./modules/bootloaders/systemd-boot.nix
     ./modules/hardware/bluetooth.nix
+    ./modules/ephemeral-root.nix
   ];
   networking.hostName = "mellinoe";
   system.stateVersion = "24.11";
