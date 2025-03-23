18379a72b72d72c8b2b2164785b43ca7c75bada3
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-24 04:09:41 +0800

    feat(stylix): add cursor

--- systems/modules/stylix.nix
+++ systems/modules/stylix.nix
@@ -1,10 +1,17 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [
     inputs.stylix.nixosModules.stylix
     ./themes/darkviolet.nix
     ./fonts/sauce-code-pro.nix
   ];
-  stylix.enable = true;
-  stylix.image = ../../media/wallpaper.jpg;
+  stylix = {
+    enable = true;
+    image = ../../media/wallpaper.jpg;
+    cursor = {
+      name = "macOS dsfs";
+      package = pkgs.apple-cursor;
+      size = 20;
+    };
+  };
 }
