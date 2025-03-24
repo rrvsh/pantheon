a394bd9ae41416726b9436b025926be1932e3fdf
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-25 07:41:42 +0800

    fix(pipewire): rm mysterious bluetooth config that was causing muffled audio

--- systems/modules/pipewire.nix
+++ systems/modules/pipewire.nix
@@ -9,19 +9,5 @@
       enable = true;
       support32Bit = true;
     };
-    # wwireplumber is enabled by default
-    wireplumber.extraConfig.bluetoothEnhancements = {
-      "monitor.bluez.properties" = {
-        "bluez5.enable-sbc-xq" = true;
-        "bluez5.enable-msbc" = true;
-        "bluez5.enable-hw-volume" = true;
-        "bluez5.roles" = [
-          "hsp_hs"
-          "hsp_ag"
-          "hfp_hf"
-          "hfp_ag"
-        ];
-      };
-    };
   };
 }
