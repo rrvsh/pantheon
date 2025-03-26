8249121529e6c5793d00f7b26a067b8a44695f05
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-26 22:18:53 +0800

    fix: merge

--- systems/modules/common.nix
+++ systems/modules/common.nix
@@ -56,21 +56,6 @@
 
   i18n.defaultLocale = "en_SG.UTF-8";
 
-<<<<<<< HEAD
-=======
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_SG.UTF-8";
-    LC_IDENTIFICATION = "en_SG.UTF-8";
-    LC_MEASUREMENT = "en_SG.UTF-8";
-    LC_MONETARY = "en_SG.UTF-8";
-    LC_NAME = "en_SG.UTF-8";
-    LC_NUMERIC = "en_SG.UTF-8";
-    LC_PAPER = "en_SG.UTF-8";
-    LC_TELEPHONE = "en_SG.UTF-8";
-    LC_TIME = "en_SG.UTF-8";
-  };
-
->>>>>>> 5a2d890e70efd93b41d8345439dffd1dabc7f5d3
   programs.nix-index-database.comma.enable = true;
 
   networking = {
