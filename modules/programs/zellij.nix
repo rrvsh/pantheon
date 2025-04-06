2c468724fd0e1f9c3439aec7df313f1216c6b6ab
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-04-06 23:46:21 +0800

    feat(zellij): move layout around

--- modules/programs/zellij.nix
+++ modules/programs/zellij.nix
@@ -1,13 +1,24 @@
 {
-  home-manager.users.rafiq.programs.zellij = {
-    enable = true;
-    attachExistingSession = true;
-    settings = {
-      show_startup_tips = false;
-      pane_frames = false;
-      keybinds.unbind = [
-        "Ctrl h"
-      ];
+  home-manager.users.rafiq = {
+    programs.zellij = {
+      enable = true;
+      attachExistingSession = true;
+      settings = {
+        show_startup_tips = false;
+        pane_frames = false;
+        keybinds.unbind = [
+          "Ctrl h"
+        ];
+      };
     };
+    xdg.configFile."zellij/layouts/default.kdl".text = # kdl
+      ''
+        layout {
+          pane
+          pane size=1 borderless=true {
+            plugin location="tab-bar"
+          }
+        }
+      '';
   };
 }
