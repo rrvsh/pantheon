020530097a32942f1173849a4c4c03c875ee7244
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-04-06 20:09:00 +0800

    feat(orpheus): add kiosk compositor cage to orpheus

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
               ./modules/networking.nix
               ./modules/shell.nix
             ];
-            desktopModules = [
+            graphicalModules = [
               ./modules/graphical.nix
             ];
           in
@@ -35,7 +35,7 @@
             specialArgs = args;
             modules =
               commonModules
-              ++ lib.optionals (type == "desktop") desktopModules
+              ++ lib.optionals (type == "graphical") graphicalModules
               # Options for specific hostnames.
               ++ (lib.optionals (hostname == "nemesis") [
                 ./modules/bootloaders/systemd-boot.nix
@@ -54,23 +54,39 @@
               ])
               ++ (lib.optionals (hostname == "orpheus") [
                 inputs.nixos-hardware.nixosModules.raspberry-pi-4
-                {
-                  fileSystems."/" = {
-                    device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
-                    fsType = "ext4";
-                  };
-                  nixpkgs.hostPlatform = "aarch64-linux";
-                }
+                (
+                  { pkgs, ... }:
+                  {
+                    fileSystems."/" = {
+                      device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+                      fsType = "ext4";
+                    };
+                    nixpkgs.hostPlatform = "aarch64-linux";
+                    services.cage = {
+                      enable = true;
+                      user = "rafiq";
+                      program = "${pkgs.firefox}/bin/firefox --kiosk -P orpheus https://www.youtube.com/watch?v=JW5meKfy3fY&autoplay=1";
+                    };
+                    home-manager.users.rafiq.programs.firefox = {
+                      enable = true;
+                      profiles.orpheus = {
+                        settings = {
+                          "media.autoplay.default" = 0;
+                        };
+                      };
+                    };
+                  }
+                )
               ]);
           };
       };
     in
     {
       nixosConfigurations = builtins.listToAttrs [
-        (mkSystem "desktop" "nemesis"
+        (mkSystem "graphical" "nemesis"
           "nvme-nvme.c0a9-323332354536453737343334-435432303030503353534438-00000001"
         )
-        (mkSystem "desktop" "mellinoe" "/dev/disk/by-id/nvme-eui.01000000000000008ce38e04019a68ab")
+        (mkSystem "graphical" "mellinoe" "/dev/disk/by-id/nvme-eui.01000000000000008ce38e04019a68ab")
         (mkSystem "headless" "apollo" "/dev/disk/by-id/nvme-eui.002538d221b47b01")
         (mkSystem "headless" "orpheus" "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888")
       ];
