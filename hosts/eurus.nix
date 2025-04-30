2a0b35b2ecfc57c79b5da7aae0c0e96ce13af89b
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-04-30 22:34:27 +0800

    feat(eurus): add static ip and dns servers

--- hosts/eurus.nix
+++ hosts/eurus.nix
@@ -4,4 +4,18 @@
   ];
   boot-config.bootloader = "systemd-boot";
   hardware-config.cpu = "amd";
+  networking = {
+    interfaces.enp3s0.useDHCP = false; # Disable DHCP, we use static IP
+    interfaces.enp3s0.ipv4.addresses = [
+      {
+        address = "160.191.77.168";
+        prefixLength = 24; # Or 255.255.255.0
+      }
+    ];
+    defaultGateway = "160.191.77.1"; # The gateway from the admin panel
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+    ]; # DNS servers from the admin panel
+  };
 }
