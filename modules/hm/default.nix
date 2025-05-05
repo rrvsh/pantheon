7d06874df6f60e7f3a551a462189edff6a8c8c4a
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-05-05 23:24:45 +0800

    chore(hmModules): add options for main apps WIP:

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -19,6 +19,26 @@ in
   options = {
     "${moduleName}" = {
       enable = lib.mkEnableOption "Enable ${moduleName}.";
+      mainApps = {
+        terminal = lib.mkOption {
+          type = lib.types.str;
+          default = "kitty";
+          example = "kitty";
+          description = "What terminal is the default.";
+        };
+        browser = lib.mkOption {
+          type = lib.types.str;
+          default = "firefox";
+          example = "firefox";
+          description = "What browser is the default.";
+        };
+        launcher = lib.mkOption {
+          type = lib.types.str;
+          default = "fuzzel";
+          example = "fuzzel";
+          description = "What launcher is the default.";
+        };
+      };
     };
   };
 
