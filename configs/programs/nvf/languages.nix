604432fd74563757858a4e09851aeb722af920c7
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-11 23:37:27 +0800

    fix(nvf): change options to vim.lsp.enable to remove deprecated option

--- configs/programs/nvf/languages.nix
+++ configs/programs/nvf/languages.nix
@@ -1,34 +1,36 @@
 {
-  programs.nvf.settings.vim.languages = {
-    # The below settings enable defaults for all languages
-    enableDAP = true;
-    enableExtraDiagnostics = true;
-    enableFormat = true;
-    enableLSP = true;
-    enableTreesitter = true;
+  programs.nvf.settings.vim = {
+    lsp.enable = true;
+    languages = {
+      # The below settings enable defaults for all languages
+      enableDAP = true;
+      enableExtraDiagnostics = true;
+      enableFormat = true;
+      enableTreesitter = true;
 
-    # Enable specific languages
-    clang.enable = true;
-    css.enable = true;
-    lua.enable = true;
-    markdown.enable = true;
-    markdown.extensions.render-markdown-nvim.enable = true;
-    nix.enable = true;
-    nix.format.type = "nixfmt";
-    python.enable = true;
-    python.lsp.server = "python-lsp-server";
-    rust.enable = true;
-    rust.crates.enable = true;
-    ts.enable = true;
+      # Enable specific languages
+      clang.enable = true;
+      css.enable = true;
+      lua.enable = true;
+      markdown.enable = true;
+      markdown.extensions.render-markdown-nvim.enable = true;
+      nix.enable = true;
+      nix.format.type = "nixfmt";
+      python.enable = true;
+      python.lsp.server = "python-lsp-server";
+      rust.enable = true;
+      rust.crates.enable = true;
+      ts.enable = true;
 
-    # Did not really check these
-    bash.enable = true;
-    csharp.enable = true;
-    go.enable = true;
-    haskell.enable = true;
-    html.enable = true;
-    java.enable = true;
-    sql.enable = true;
-    yaml.enable = true;
+      # Did not really check these
+      bash.enable = true;
+      csharp.enable = true;
+      go.enable = true;
+      haskell.enable = true;
+      html.enable = true;
+      java.enable = true;
+      sql.enable = true;
+      yaml.enable = true;
+    };
   };
 }
