8b69346b1f7178702af0fceb81bec595682ac349
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-18 17:21:23 +0800

    feat(homes/rafiq): add rebuild script

--- homes/x86_64-linux/rafiq/default.nix
+++ homes/x86_64-linux/rafiq/default.nix
@@ -15,6 +15,17 @@
     neovim
     ripgrep
     kitty
+    (pkgs.writeShellScriptBin "rebuild" # sh
+    ''
+    if [ ! -f "flake.nix" ]; then
+    	echo "flake.nix not found in current directory. exiting..."
+	exit 1
+    fi
+
+    git add .
+    nixos-rebuild switch --flake . --use-remote-sudo
+    git commit -a
+    '')
   ];
   home.sessionVariables = {
     EDITOR = "nvim";
