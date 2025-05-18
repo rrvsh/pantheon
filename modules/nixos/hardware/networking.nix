343dd8fffc47aaa598eb29e35139b4b785e08fe2
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-18 23:08:53 +0800

    feat(modules/networking): enable tailscale

--- modules/nixos/hardware/networking.nix
+++ modules/nixos/hardware/networking.nix
@@ -5,6 +5,10 @@
   networking.useDHCP = lib.mkDefault true;
   networking.hostName = config.system.hostname;
   networking.networkmanager.enable = true;
+
+  services.tailscale = {
+  enable = true;
+  authKeyFile = config.sops.secrets."keys/tailscale".path;};
     }
   ];
 }
--- modules/nixos/system/secrets.nix
+++ modules/nixos/system/secrets.nix
@@ -4,6 +4,7 @@
     defaultSopsFile = lib.snowfall.fs.get-file "secrets/secrets.yaml";
     age.sshKeyPaths = ["/persist/home/rafiq/.ssh/id_ed25519"];
     secrets = {
+      "keys/tailscale" = {};
       "rafiq/hashedPassword".neededForUsers = true;
     };
   };
