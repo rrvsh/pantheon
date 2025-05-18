cbd59b8d35797cf71a785526519140329d7c6acc
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-18 23:11:59 +0800

    feat(modules/networking): enable ssh

--- modules/nixos/hardware/networking.nix
+++ modules/nixos/hardware/networking.nix
@@ -6,6 +6,12 @@
   networking.hostName = config.system.hostname;
   networking.networkmanager.enable = true;
 
+  services.openssh = {
+enable = true;
+settings = {
+PrintMotd = true;};
+  };
+
   services.tailscale = {
   enable = true;
   authKeyFile = config.sops.secrets."keys/tailscale".path;};
