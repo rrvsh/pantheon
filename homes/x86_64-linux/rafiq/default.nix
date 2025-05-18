2e466e6ecad2158beded13931daa785dc7f8410f
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-18 16:33:19 +0800

    feat(homes/rafiq): add git config

--- homes/x86_64-linux/rafiq/default.nix
+++ homes/x86_64-linux/rafiq/default.nix
@@ -3,15 +3,14 @@
 {
   home.stateVersion = "24.11";
 
+  home.file = {
+  };
+
   home.packages = with pkgs; [
     git
     neovim
     ripgrep
   ];
-
-  home.file = {
-  };
-
   home.sessionVariables = {
     EDITOR = "nvim";
   };
@@ -21,5 +20,28 @@ directories = [
 ".ssh"
 ];
 allowOther = true;
+  };
+
+  home.sessionVariables.GIT_CONFIG_GLOBAL = "$HOME/.config/git/config";
+  home.shellAliases = {
+    gs = "git status";
+    gc = "git commit";
+    gcam = "git commit -am";
+    gu = "git push";
+    gy = "git pull";
+  };
+  programs.git = {
+    enable = true;
+    userName = "Mohammad Rafiq";
+    userEmail = "rafiq@rrv.sh";
+    signing.key = "~/.ssh/id_ed25519.pub";
+    signing.signByDefault = true;
+    extraConfig = {
+      init.defaultBranch = "prime";
+      push.autoSetupRemote = true;
+      pull.rebase = false;
+      core.editor = "$EDITOR";
+      gpg.format = "ssh";
+    };
   };
 }
---
+++ modules/hm/cli/default.nix
@@ -0,0 +1,13 @@
+{config, lib, osConfig, ... }:
+{
+  imports = [
+  ];
+
+  options.cli =	{
+
+  };
+
+  config = {
+
+  };
+}
