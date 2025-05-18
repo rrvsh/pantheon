1bf0680ac46757811f1269d19910e641c27b378f
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-18 16:44:54 +0800

    refactor(homes/rafiq): move git config to its own module

--- homes/x86_64-linux/rafiq/default.nix
+++ homes/x86_64-linux/rafiq/default.nix
@@ -1,13 +1,16 @@
 { config, pkgs, osConfig, ... }:
 
 {
+  imports = [
+    ./programs/git.nix
+  ];
+
   home.stateVersion = "24.11";
 
   home.file = {
   };
 
   home.packages = with pkgs; [
-    git
     neovim
     ripgrep
   ];
@@ -21,28 +24,5 @@ directories = [
 "repos"
 ];
 allowOther = true;
-  };
-
-  home.sessionVariables.GIT_CONFIG_GLOBAL = "$HOME/.config/git/config";
-  home.shellAliases = {
-    gs = "git status";
-    gc = "git commit";
-    gcam = "git commit -am";
-    gu = "git push";
-    gy = "git pull";
-  };
-  programs.git = {
-    enable = true;
-    userName = "Mohammad Rafiq";
-    userEmail = "rafiq@rrv.sh";
-    signing.key = "~/.ssh/id_ed25519.pub";
-    signing.signByDefault = true;
-    extraConfig = {
-      init.defaultBranch = "prime";
-      push.autoSetupRemote = true;
-      pull.rebase = false;
-      core.editor = "$EDITOR";
-      gpg.format = "ssh";
-    };
   };
 }
