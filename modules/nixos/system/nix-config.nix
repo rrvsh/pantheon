511990314cf51095ee8de74b49f9bc12bf316e58
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-19 18:35:58 +0800

    feat(modules/nixos/system): add @wheel to trusted users for nix

--- modules/nixos/system/nix-config.nix
+++ modules/nixos/system/nix-config.nix
@@ -1,6 +1,16 @@
 { config, lib, ... }:
 {
   config = {
-    nix.settings.experimental-features = ["nix-command" "flakes"];
+    nixpkgs.config.allowUnfree = true;
+
+    nix.settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "pipe-operators"
+      ];
+
+      trusted-users = [ "@wheel" ];
+    };
   };
 }
