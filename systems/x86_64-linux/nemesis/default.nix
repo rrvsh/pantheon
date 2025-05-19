51f2e81fda7e96b2c95331ec1f349c3e4ff8e3ff
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-19 09:14:11 +0800

    feat(systems/nemesis): add host rsa key to persist

--- systems/x86_64-linux/nemesis/default.nix
+++ systems/x86_64-linux/nemesis/default.nix
@@ -31,6 +31,8 @@ refresh-rate = "144";
     files = [
       "/etc/ssh/ssh_host_ed25519_key"
       "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
       "/etc/machine-id"
     ];
   };
