d750b9baa29400d97edbd17c87d07b61066c7e53
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-19 18:40:43 +0800

    feat: add devenv and direnv

--- homes/x86_64-linux/rafiq/default.nix
+++ homes/x86_64-linux/rafiq/default.nix
@@ -21,8 +21,15 @@
   home.packages = with pkgs; [
     ripgrep
     fzf
+    devenv
     pantheon.rebuild
   ];
+
+    programs.direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+
   home.persistence."/persist/home/rafiq" = {
 	directories = [
 	".ssh"
