95146e2cdf7e6d84229edcb81ecc2939de1fd68d
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-20 11:32:27 +0800

    feat(nvf): add keymap to create file under cursor

--- modules/home/cli/editor/nvf.nix
+++ modules/home/cli/editor/nvf.nix
@@ -5,6 +5,15 @@
     programs.nvf = {
       enable = true;
       settings.vim = {
+      keymaps = [
+        {
+          desc = "Open the file path under the cursor, making the file if it doesn't exist.";
+          key = "gf";
+          mode = "n";
+          action = ":cd %:p:h<CR>:e <cfile><CR>";
+          silent = true;
+        }
+      ];
         utility.yazi-nvim = {
           enable = true;
 	  mappings = {
