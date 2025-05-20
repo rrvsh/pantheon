48c80b9d9574d9afc274c1d6a022a643e0e6a466
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-20 19:29:36 +0800

    feat(nixosModules/hardware): add cuda binary cache

--- modules/nixos/hardware/nvidia.nix
+++ modules/nixos/hardware/nvidia.nix
@@ -5,24 +5,25 @@
   ...
 }:
 {
-  config = lib.mkIf (config.hardware.gpu == "nvidia") (
-    lib.mkMerge [
-      {
-        #TODO: Setup CUDA
-        hardware.graphics.enable = true;
-        hardware.graphics.extraPackages = with pkgs; [
-          nvidia-vaapi-driver
-        ];
-        services.xserver.videoDrivers = [ "nvidia" ];
-        hardware.nvidia.open = true;
-        hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
-        nixpkgs.config.allowUnfree = true;
-        environment.variables = {
-          LIBVA_DRIVER_NAME = "nvidia";
-          __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-          NVD_BACKEND = "direct";
-        };
-      }
-    ]
-  );
+  config = lib.mkIf (config.hardware.gpu == "nvidia") {
+    #TODO: Setup CUDA
+    hardware.graphics.enable = true;
+    hardware.graphics.extraPackages = with pkgs; [
+      nvidia-vaapi-driver
+    ];
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia.open = true;
+    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
+    nixpkgs.config.allowUnfree = true;
+    environment.variables = {
+      LIBVA_DRIVER_NAME = "nvidia";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      NVD_BACKEND = "direct";
+    };
+
+    nix.settings.substituters = [ "cuda-maintainers.cachix.org" ];
+    nix.settings.trusted-public-keys = [
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
+  };
 }
