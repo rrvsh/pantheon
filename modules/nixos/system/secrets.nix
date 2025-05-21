d1c8502873ce3d61264095e04d8dd3cb8e834a9e
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-21 16:46:33 +0800

    feat(nixosModules/system): add cvt jira env vars

--- modules/nixos/system/secrets.nix
+++ modules/nixos/system/secrets.nix
@@ -6,11 +6,15 @@
     secrets = {
       "keys/tailscale" = { };
       "keys/gemini" = { };
+      "keys/cvt-jira" = { };
+      "misc/cvt-jira-link" = { };
       "rafiq/hashedPassword".neededForUsers = true;
     };
   };
   environment.shellInit = # sh
     ''
       export GEMINI_API_KEY=$(sudo cat ${config.sops.secrets."keys/gemini".path})
+      export CVT_JIRA_KEY=$(sudo cat ${config.sops.secrets."keys/cvt-jira".path})
+      export CVT_JIRA_LINK=$(sudo cat ${config.sops.secrets."misc/cvt-jira-link".path})
     '';
 }
