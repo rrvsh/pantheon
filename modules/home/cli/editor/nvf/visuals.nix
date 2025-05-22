9334f9daf0a1153c740d1a486170d0173b6e79e3
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-22 14:14:47 +0800

    feat(nvf): add visual options

--- modules/home/cli/editor/nvf/visuals.nix
+++ modules/home/cli/editor/nvf/visuals.nix
@@ -1,3 +1,8 @@
 {
   indent-blankline.enable = true;
+  fidget-nvim.enable = true;
+  highlight-undo.enable = true;
+  nvim-cursorline.enable = true;
+  nvim-web-devicons.enable = true;
+  rainbow-delimiters.enable = true;
 }
