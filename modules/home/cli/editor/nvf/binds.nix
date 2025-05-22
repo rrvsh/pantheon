bc55e5efb6dbb71d03a396ea5c8eada4d0f6f871
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-22 14:06:31 +0800

    fix(hardtime-nvim): make arrowkeys work in insert mode

--- modules/home/cli/editor/nvf/binds.nix
+++ modules/home/cli/editor/nvf/binds.nix
@@ -1,4 +1,26 @@
 {
-  hardtime-nvim.enable = true;
+  hardtime-nvim = {
+    enable = true;
+    setupOpts = {
+      disabled_keys = {
+        "<Up>" = [
+          "n"
+          "x"
+        ];
+        "<Down>" = [
+          "n"
+          "x"
+        ];
+        "<Left>" = [
+          "n"
+          "x"
+        ];
+        "<Right>" = [
+          "n"
+          "x"
+        ];
+      };
+    };
+  };
   whichKey.enable = true;
 }
