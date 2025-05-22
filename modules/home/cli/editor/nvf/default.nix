0bee56e7e8afa0574ada50a1baa420670ed5d7b4
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-22 14:27:23 +0800

    feat(nvf): add undofile

--- modules/home/cli/editor/nvf/default.nix
+++ modules/home/cli/editor/nvf/default.nix
@@ -2,9 +2,13 @@
 {
   config = lib.mkIf (config.cli.editor == "nvf") {
     home.sessionVariables.EDITOR = "nvim";
+    home.persistence."/persist/home/${config.snowfallorg.user.name}".directories = [
+      ".local/share/nvf"
+    ];
     programs.nvf = {
       enable = true;
       settings.vim = {
+        undoFile.enable = true;
         autocomplete = import ./autocomplete.nix;
         binds = import ./binds.nix;
         keymaps = import ./keymaps.nix;
