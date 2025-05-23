946b5aac276263974cb38d983e72091917cf6119
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-24 00:35:21 +0800

    feat(nvf): add some easy motions

--- modules/home/cli/editor/nvf/keymaps.nix
+++ modules/home/cli/editor/nvf/keymaps.nix
@@ -1,12 +1,5 @@
 [
   {
-    desc = "";
-    key = "f";
-    mode = "n";
-    action = ":FzfLua files<CR>";
-    silent = true;
-  }
-  {
     desc = "Open the file path under the cursor, making the file if it doesn't exist.";
     key = "gf";
     mode = "n";
--- modules/home/cli/editor/nvf/utility.nix
+++ modules/home/cli/editor/nvf/utility.nix
@@ -1,10 +1,9 @@
 {
-  direnv.enable = true;
+  ccc.enable = true;
   leetcode-nvim.enable = true;
   mkdir.enable = true;
   motion.leap.enable = true;
-  motion.leap.mappings.leapForwardTo = "s";
-  motion.leap.mappings.leapBackwardTo = "S";
+  motion.hop.enable = true;
   vim-wakatime.enable = true;
   yazi-nvim = {
     enable = true;
