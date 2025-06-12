e65b478d21db3a3e84467cdb675a448919b754a5
Mohammad Rafiq <rafiq@rrv.sh>
2025-06-13 04:54:51 +0800

    chore(web-servers): remove staging letsencrypt server

--- modules/nixos/server/web-servers/default.nix
+++ modules/nixos/server/web-servers/default.nix
@@ -19,8 +19,6 @@ in
         acceptTerms = true;
         defaults = {
           inherit (config.system.mainUser) email;
-          #TODO: switch back to production environment
-          server = "https://acme-staging-v02.api.letsencrypt.org/directory";
           dnsProvider = "cloudflare";
           credentialFiles."CLOUDFLARE_DNS_API_TOKEN_FILE" = config.sops.secrets."keys/cloudflare".path;
         };
