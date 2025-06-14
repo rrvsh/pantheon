2387ad909971fca6baddd9728dfbc276c0140478
Mohammad Rafiq <rafiq@rrv.sh>
2025-06-14 18:03:11 +0800

    feat(cli): remove cli module and move zsh config to systems

--- systems/x86_64-linux/common.nix
+++ systems/x86_64-linux/common.nix
@@ -1,3 +1,7 @@
+{ pkgs, ... }:
+let
+  inherit (pkgs) zsh;
+in
 {
   system.mainUser = {
     name = "rafiq";
@@ -5,5 +9,12 @@
     email = "rafiq@rrv.sh";
   };
   server.mountHelios = true;
+
+  users.defaultUserShell = zsh;
+  programs = {
+    zsh.enable = true;
+    zsh.enableCompletion = true;
+  };
+
   nixpkgs.hostPlatform = "x86_64-linux";
 }
