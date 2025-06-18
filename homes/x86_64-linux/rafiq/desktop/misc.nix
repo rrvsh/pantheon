fad78517c9f4e51c3532ab1f55e76fd98a2e9bae
Mohammad Rafiq <rafiq@rrv.sh>
2025-06-19 06:01:50 +0800

    feat(vesktop): enable vesktop and add empty settings

--- homes/x86_64-linux/rafiq/desktop/misc.nix
+++ homes/x86_64-linux/rafiq/desktop/misc.nix
@@ -8,6 +8,13 @@
   ];
   programs = {
     obs-studio.enable = true;
+    vesktop = {
+      enable = true;
+      # https://github.com/Vencord/Vesktop/blob/main/src/shared/settings.d.ts
+      settings = { };
+      # https://github.com/Vendicated/Vencord/blob/main/src/api/Settings.ts
+      vencord.settings = { };
+    };
     thunderbird.enable = true;
     thunderbird.profiles.rafiq.isDefault = true;
   };
