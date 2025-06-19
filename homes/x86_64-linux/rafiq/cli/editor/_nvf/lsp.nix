fe43d32cd26aa42d54dd85bc8692e215f6c1ab5d
Mohammad Rafiq <rafiq@rrv.sh>
2025-06-19 10:41:20 +0800

    feat(editor): configure otter-nvim with setup options for buffers and whitespace

--- homes/x86_64-linux/rafiq/cli/editor/_nvf/lsp.nix
+++ homes/x86_64-linux/rafiq/cli/editor/_nvf/lsp.nix
@@ -5,5 +5,12 @@
   lightbulb.enable = true;
   lspkind.enable = true;
   null-ls.enable = true;
-  otter-nvim.enable = true;
+  otter-nvim = {
+    enable = true;
+    setupOpts = {
+      buffers.set_filetype = true;
+      buffers.write_to_disk = true;
+      handle_leading_whitespace = true;
+    };
+  };
 }
