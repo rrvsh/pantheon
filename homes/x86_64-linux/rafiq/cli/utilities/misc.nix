0521bb516deef8ae82d61adf208609ddc684a848
Mohammad Rafiq <rafiq@rrv.sh>
2025-06-22 14:29:16 +0800

    feat(home): move starship config to its own module

--- homes/x86_64-linux/rafiq/cli/utilities/misc.nix
+++ homes/x86_64-linux/rafiq/cli/utilities/misc.nix
@@ -12,22 +12,6 @@
       enable = true;
       nix-direnv.enable = true;
     };
-    starship = {
-      enable = true;
-      settings = {
-        add_newline = false;
-        format = ''
-          $directory$character
-        '';
-        right_format = ''
-          $all
-        '';
-        git_branch.format = "[$symbol$branch(:$remote_branch)]($style) ";
-        shlvl.disabled = false;
-        hostname.disabled = true;
-        username.disabled = true;
-      };
-    };
   };
   home = {
     shellAliases = {
---
+++ homes/x86_64-linux/rafiq/cli/utilities/starship.nix
@@ -0,0 +1,27 @@
+{ lib, ... }:
+let
+  inherit (lib.strings) concatStrings;
+in
+{
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      format = concatStrings [
+        # First Line
+        ## Left Prompt
+        "$hostname$directory"
+        "$fill"
+        ## Right Prompt
+        "$all"
+        # Second Line
+        ## Left Prompt
+        "$character"
+      ];
+      git_branch.format = "[$symbol$branch(:$remote_branch)]($style) ";
+      shlvl.disabled = false;
+      username.disabled = true;
+      fill.symbol = " ";
+    };
+  };
+}
