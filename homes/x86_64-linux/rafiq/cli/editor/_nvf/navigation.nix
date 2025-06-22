8a4f136d027cb55453ac28fbc2471804dea3c772
Mohammad Rafiq <rafiq@rrv.sh>
2025-06-23 05:20:07 +0800

    fix(nvf): remap harpoon mappings to use "mm" and "ml"

--- homes/x86_64-linux/rafiq/cli/editor/_nvf/navigation.nix
+++ homes/x86_64-linux/rafiq/cli/editor/_nvf/navigation.nix
@@ -1,8 +1,8 @@
 {
   harpoon = {
     enable = true;
-    mappings.listMarks = "<leader>hl";
-    mappings.markFile = "<leader>hm";
+    mappings.listMarks = "<leader>ml";
+    mappings.markFile = "<leader>mm";
     setupOpts.defaults.save_on_toggle = true;
     setupOpts.defaults.sync_on_ui_close = true;
   };
