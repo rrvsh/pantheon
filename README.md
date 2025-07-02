6e7fc72d38a37665cf650bcebbcd93593a6157d4
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-03 07:38:05 +0800

    feat(files): generate list of generated files dynamically

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 This flake uses the [files flake-parts module](https://flake.parts/options/files.html) to generate documentation.
 The list of generated files are:
-
+- [README.md](README.md)
 The option `text.<name> supports either a string or a submodule with attributes order and parts. The parts attribute can either be a string, which will get concatenated in the order laid out in `text.<name>.order`, or can itself have the attributes order and parts, in which case it will be evaluated recursively.
--- modules/flake-parts/files.nix
+++ modules/flake-parts/files.nix
@@ -1,11 +1,29 @@
-{ inputs, ... }:
+{
+  inputs,
+  withSystem,
+  lib,
+  config,
+  ...
+}:
+let
+  inherit (builtins) map head;
+  inherit (lib) concatStringsSep;
+  #TODO: add the .nix file its generated from
+  mkListEntry = x: "- [" + x.path_ + "](" + x.path_ + ")";
+  listOfGeneratedFiles = withSystem (head config.systems) (psArgs: psArgs.config.files.files);
+in
 {
   imports = [ inputs.files.flakeModules.default ];
   perSystem = psArgs: {
     make-shells.default.packages = [ psArgs.config.files.writer.drv ];
   };
-  text.readme.parts.generated-files = ''
-    This flake uses the [files flake-parts module](https://flake.parts/options/files.html) to generate documentation.
-    The list of generated files are:
-  '';
+  text.readme.parts.generated-files = concatStringsSep "\n" (
+    [
+      "This flake uses the [files flake-parts module](https://flake.parts/options/files.html) to generate documentation."
+
+      "The list of generated files are:"
+
+    ]
+    ++ (map mkListEntry listOfGeneratedFiles)
+  );
 }
