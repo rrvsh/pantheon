8eceb81ad99e4533c8f44b61f8e5305431dbf6d1
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-04 18:42:59 +0800

    feat(homes/rafiq): enable starship prompt in fish shell

--- homes/x86_64-linux/rafiq/cli/shell.nix
+++ homes/x86_64-linux/rafiq/cli/shell.nix
@@ -2,6 +2,7 @@
 let
   inherit (builtins) toString;
   inherit (lib) getExe mkOrder;
+  inherit (lib.strings) concatStrings;
   screensaverTimeout = toString 100;
   screensaverCommand = "${getExe pkgs.cbonsai} -S -w 0.1 -L 40 -M 2 -b 2";
 in
@@ -9,6 +10,28 @@ in
   home.shell.enableShellIntegration = true;
   home.sessionVariables.SHELL = "fish";
   programs.fish.enable = true;
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      format = concatStrings [
+        # First Line
+        ## Left Prompt
+        "$hostname$directory"
+        "$fill"
+        ## Right Prompt
+        "$all"
+        # Second Line
+        ## Left Prompt
+        "$character"
+      ];
+      git_branch.format = "[$symbol$branch(:$remote_branch)]($style) ";
+      shlvl.disabled = false;
+      username.disabled = true;
+      fill.symbol = " ";
+    };
+  };
+  # figure out for fish
   programs.zsh.initContent =
     mkOrder 1200
       # zsh
