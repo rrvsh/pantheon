df65236c270121fb07d91facd287cc5b8fe80d7f
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-05 00:16:35 +0800

    feat(homes/rafiq): add note bash script to zk notes cli

--- homes/x86_64-linux/rafiq/cli/utilities/zk.nix
+++ homes/x86_64-linux/rafiq/cli/utilities/zk.nix
@@ -1,9 +1,19 @@
+{ pkgs, ... }:
 {
   persistDirs = [ "notebook" ];
   programs.zk = {
     enable = true;
-    settings = {
-      notebook.dir = "~/notebook";
-    };
+    settings.notebook.dir = "~/notebook";
   };
+  home.packages = [
+    (pkgs.writeShellScriptBin "note" # bash
+      ''
+        zk edit -i
+        pushd ~/notebook
+        git add .
+        commit
+        popd
+      ''
+    )
+  ];
 }
