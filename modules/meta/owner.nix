691b9251489d43108485bb8f62345ed90ab42e5c
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-06 05:20:31 +0800

    refactor: move docs/readme and docs/cheatsheet to flake-parts

--- modules/meta/owner.nix
+++
@@ -1,7 +0,0 @@
-{
-  flake.meta.owner = {
-    username = "rafiq";
-    email = "rafiq@rrv.sh";
-    pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdsZyY3gu8IGB8MzMnLdh+ClDxQQ2RYG9rkeetIKq8n";
-  };
-}
