b43476ccee5b863593358e5646ef7929d97209d7
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-06 04:18:21 +0800

    refactor(nixos): change hostSpec to define top level option

---
+++ modules/configurations/nixos.nix
@@ -0,0 +1,28 @@
+{ config, lib, ... }:
+let
+  inherit (lib.trivial) pipe;
+  inherit (lib.attrsets) filterAttrs mapAttrs';
+  inherit (lib.strings) removePrefix hasPrefix;
+  prefix = "nixos/";
+  hosts = config.flake.hostSpec.hosts or { };
+  mkSystem =
+    name: value:
+    let
+      hostName = removePrefix prefix name;
+    in
+    {
+      name = hostName;
+      value = lib.nixosSystem {
+        modules = [
+          value
+          { networking = { inherit hostName; }; }
+        ];
+      };
+    };
+in
+{
+  flake.nixosConfigurations = pipe hosts [
+    (filterAttrs (name: _: hasPrefix prefix name))
+    (mapAttrs' mkSystem)
+  ];
+}
