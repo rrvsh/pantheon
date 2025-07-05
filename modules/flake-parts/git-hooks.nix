2599d2e49c141150ba848746cb32207eb3b48950
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-05 20:30:31 +0800

    feat(git-hooks): enable nixpkgs-fmt pre-commit hook

--- modules/flake-parts/git-hooks.nix
+++ modules/flake-parts/git-hooks.nix
@@ -2,4 +2,11 @@
 {
   # TODO: add to readme
   imports = [ inputs.git-hooks.flakeModule ];
+  perSystem = psArgs: {
+    pre-commit.settings.hooks = {
+      nixpkgs-fmt.enable = true;
+      #TODO: add write-files
+    };
+    make-shells.default.shellHook = psArgs.config.pre-commit.installationScript;
+  };
 }
