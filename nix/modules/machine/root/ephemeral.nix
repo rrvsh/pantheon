fe91044c8b9a223cb860d78682efdd9739288bb3
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-07 17:47:51 +0800

    refactor(nixos): move persisted ssh host keys to ssh module

--- nix/modules/machine/root/ephemeral.nix
+++ nix/modules/machine/root/ephemeral.nix
@@ -17,13 +17,6 @@ in
               btrfs subvolume delete "/btrfs_tmp/root"
             fi
           '';
-          persistFiles = [
-            #TODO: move to ssh config
-            "/etc/ssh/ssh_host_ed25519_key"
-            "/etc/ssh/ssh_host_ed25519_key.pub"
-            "/etc/ssh/ssh_host_rsa_key"
-            "/etc/ssh/ssh_host_rsa_key.pub"
-          ];
         })
       ];
     };
---
+++ nix/modules/ssh.nix
@@ -0,0 +1,10 @@
+{
+  flake.modules.nixos.default = {
+    persistFiles = [
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+    ];
+  };
+}
