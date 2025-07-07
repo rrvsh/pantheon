03fca8b28b6deb79e0f4e96267eb4d9aaa889eea
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-07 19:24:36 +0800

    refactor(modules): clean up folder structure

--- nix/modules/machine/default.nix
+++ nix/modules/machine/default.nix
@@ -1,7 +1,38 @@
+{ lib, ... }:
+let
+  inherit (lib.options) mkEnableOption;
+  inherit (lib.modules) mkIf mkMerge;
+in
 {
   flake.modules.nixos.default =
-    { modulesPath, ... }:
+    { config, modulesPath, ... }:
+    let
+      cfg = config.machine;
+    in
     {
       imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+      options.machine.bluetooth.enable = mkEnableOption "";
+      options.machine.usb.automount = mkEnableOption "";
+      config = mkMerge [
+        (mkIf cfg.usb.automount {
+          services.udisks2.enable = true;
+          home-manager.sharedModules = [
+            {
+              services.udiskie = {
+                enable = true;
+                automount = true;
+                notify = true;
+              };
+            }
+          ];
+        })
+        (mkIf cfg.bluetooth.enable {
+          persistDirs = [ "/var/lib/bluetooth" ];
+          hardware.bluetooth = {
+            enable = true;
+            settings.General.Experimental = true;
+          };
+        })
+      ];
     };
 }
