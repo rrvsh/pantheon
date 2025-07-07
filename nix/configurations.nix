b63959f307ce6d568ed988901921bd99f726dde6
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-07 18:11:33 +0800

    refactor(configurations): centralise home-manager config

--- nix/configurations.nix
+++ nix/configurations.nix
@@ -7,7 +7,14 @@
 let
   inherit (lib) nixosSystem;
   inherit (lib.attrsets) mapAttrs;
+  inherit (cfg.lib.modules) forAllUsers';
   cfg = config.flake;
+  globalCfg = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    sharedModules = [ cfg.modules.homeManager.default ];
+    users = forAllUsers' (name: _: cfg.homes.${name});
+  };
   hosts = cfg.manifest.hosts or { };
   mkConfigurations =
     class: hosts:
@@ -15,13 +22,11 @@ let
       name: value:
       if class == "nixos" then
         nixosSystem {
-          specialArgs = {
-            inherit inputs;
-            inherit (cfg) manifest;
-            hostName = name;
-          };
+          specialArgs.hostName = name;
           modules = [
             cfg.modules.nixos.default
+            inputs.home-manager.nixosModules.home-manager
+            { home-manager = globalCfg; }
             (value.extraCfg or { })
           ];
         }
@@ -30,5 +35,6 @@ let
     ) hosts;
 in
 {
+  imports = [ inputs.home-manager.flakeModules.home-manager ];
   flake.nixosConfigurations = mkConfigurations "nixos" hosts.nixos;
 }
