5d24a1199053cea2a759f7e64eef735a135873ca
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-07 15:18:33 +0800

    feat(lib): add admin user using custom lib function

--- nix/flake-parts/meta.nix
+++ nix/flake-parts/meta.nix
@@ -1,8 +1,16 @@
-{ lib, inputs, ... }:
+{
+  lib,
+  config,
+  inputs,
+  ...
+}:
 let
   inherit (lib.options) mkOption;
   inherit (lib.types) path lazyAttrsOf raw;
   inherit (inputs.flake-parts.lib) mkSubmoduleOptions;
+  inherit (cfg.lib.attrsets) firstAttrNameMatching;
+  cfg = config.flake;
+  username = firstAttrNameMatching (_: v: v.primary or false) cfg.manifest.users;
 in
 {
   options.flake = mkSubmoduleOptions {
@@ -14,5 +22,13 @@ in
       type = path;
       default = "";
     };
+    admin = mkOption {
+      type = lazyAttrsOf raw;
+      default = { };
+    };
+  };
+
+  config.flake.admin = cfg.manifest.users.${username} // {
+    inherit username;
   };
 }
