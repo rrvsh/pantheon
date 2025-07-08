4507f4b1133222e1cf4fd5e01b5cfc3e1904c9a1
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-09 01:39:00 +0800

    feat(homes/rafiq): add hyprlock config and pam service fix

--- nix/homes/rafiq/desktop/default.nix
+++ nix/homes/rafiq/desktop/default.nix
@@ -7,6 +7,7 @@
     "steam-unwrapped"
   ];
   flake.modules.nixos.graphical = {
+    security.pam.services.hyprlock = { };
     programs.steam = {
       enable = true;
       gamescopeSession.enable = true;
@@ -92,31 +93,34 @@
           enable = true;
           inherit profiles;
         };
-        hyprlock.settings = {
-          general.hide_cursor = true;
-          general.ignore_empty_input = true;
-          background.blur_passes = 5;
-          background.blur_size = 5;
-          label = {
-            text = ''hi, $USER.'';
-            font_size = 32;
-            position = "0, 0";
-            halign = "center";
-            valign = "center";
-            zindex = 1;
-            shadow_passes = 5;
-            shadow_size = 5;
-          };
-          input-field = {
-            placeholder_text = "";
-            fade_on_empty = true;
-            size = "200, 45";
-            position = "0, -5%";
-            halign = "center";
-            valign = "center";
-            zindex = 1;
-            shadow_passes = 5;
-            shadow_size = 5;
+        hyprlock = {
+          enable = true;
+          settings = {
+            general.hide_cursor = true;
+            general.ignore_empty_input = true;
+            background.blur_passes = 5;
+            background.blur_size = 5;
+            label = {
+              text = ''hi, $USER.'';
+              font_size = 32;
+              position = "0, 0";
+              halign = "center";
+              valign = "center";
+              zindex = 1;
+              shadow_passes = 5;
+              shadow_size = 5;
+            };
+            input-field = {
+              placeholder_text = "";
+              fade_on_empty = true;
+              size = "200, 45";
+              position = "0, -5%";
+              halign = "center";
+              valign = "center";
+              zindex = 1;
+              shadow_passes = 5;
+              shadow_size = 5;
+            };
           };
         };
         ghostty = {
