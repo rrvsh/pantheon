0f741cbb368fc0ebed16c0bbb67c4746a75a3b51
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-09 09:15:37 +0800

    refactor(nix): Move modules to modules.<system>.default structure

--- nix/modules/system/system.nix
+++ nix/modules/system/system.nix
@@ -1,14 +1,17 @@
 {
-  flake.modules.nixos.default = {
-    persistFiles = [ "/etc/machine-id" ];
-    persistDirs = [ "/var/lib/systemd" ];
-    time.timeZone = "Asia/Singapore";
-    i18n.defaultLocale = "en_US.UTF-8";
-    system.stateVersion = "25.11";
-  };
-  flake.modules.homeManager.default =
-    { osConfig, ... }:
-    {
-      home.stateVersion = osConfig.system.stateVersion;
+  flake.modules = {
+    nixos.default = {
+      persistFiles = [ "/etc/machine-id" ];
+      persistDirs = [ "/var/lib/systemd" ];
+      time.timeZone = "Asia/Singapore";
+      i18n.defaultLocale = "en_US.UTF-8";
+      system.stateVersion = "25.11";
     };
+    homeManager.default =
+      { osConfig, ... }:
+      {
+        home.stateVersion = osConfig.system.stateVersion;
+      };
+    darwin.default.system.stateVersion = 6;
+  };
 }
