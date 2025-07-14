e5e365a1e0be6fd2a637df4bcc1dbb56f30ae483
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-14 08:42:34 +0800

    feat(homes): Add darwin support for user shells configuration

--- nix/modules/cli/shell.nix
+++ nix/modules/cli/shell.nix
@@ -5,19 +5,30 @@ let
   inherit (lib.attrsets) mapAttrs';
 in
 {
-  flake.modules.nixos.default =
-    { pkgs, ... }:
-    {
-      programs = mapAttrs' (name: value: {
-        name = value.shell;
-        value.enable = true;
-      }) cfg.manifest.users;
-      users.users = forAllUsers' (_: value: { shell = pkgs.${value.shell}; });
-    };
-  flake.modules.homeManager.default =
-    { config, ... }:
-    {
-      programs.${cfg.manifest.users.${config.home.username}.shell}.enable = true;
-      home.shell.enableShellIntegration = true;
-    };
+  flake.modules = {
+    nixos.default =
+      { pkgs, ... }:
+      {
+        programs = mapAttrs' (name: value: {
+          name = value.shell;
+          value.enable = true;
+        }) cfg.manifest.users;
+        users.users = forAllUsers' (_: value: { shell = pkgs.${value.shell}; });
+      };
+    darwin.default =
+      { pkgs, ... }:
+      {
+        programs = mapAttrs' (name: value: {
+          name = value.shell;
+          value.enable = true;
+        }) cfg.manifest.users;
+        users.users = forAllUsers' (_: value: { shell = pkgs.${value.shell}; });
+      };
+    homeManager.default =
+      { config, ... }:
+      {
+        programs.${cfg.manifest.users.${config.home.username}.shell}.enable = true;
+        home.shell.enableShellIntegration = true;
+      };
+  };
 }
