e72a8a05868f4e9d8adb5974a6216d7271380ebd
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-14 15:22:45 +0800

    feat(darwin): add brew to path

--- nix/homes/rafiq/desktop/darwin.nix
+++ nix/homes/rafiq/desktop/darwin.nix
@@ -16,4 +16,17 @@ in
       "slack"
     ];
   };
+  flake.modules.homeManager.rafiq = {
+    # make sure brew is on the path for M1
+    programs.zsh.initContent = ''
+      if [[ $(uname -m) == 'arm64' ]]; then
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+      fi
+    '';
+    programs.fish.shellInit = ''
+      if test (uname -m) = "arm64"
+        eval (/opt/homebrew/bin/brew shellenv)
+      end
+    '';
+  };
 }
