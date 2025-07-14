fba46e0d0de0152bf987549380afd8c0268c26a1
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-14 18:57:24 +0800

    feat(darwin): enable sudo auth with touchid

--- nix/modules/system/sudo.nix
+++ nix/modules/system/sudo.nix
@@ -8,4 +8,9 @@ in
     nix.settings.trusted-users = [ "@wheel" ];
     users.users.${cfg.admin.username}.extraGroups = [ "wheel" ];
   };
+  flake.modules.darwin.default.security.pam.services.sudo_local = {
+    enable = true;
+    reattach = true;
+    touchIdAuth = true;
+  };
 }
