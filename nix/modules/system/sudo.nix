343f3d481f7fab97726e93c22cf4f0403a31d5e9
Mohammad Rafiq <rafiq@rrv.sh>
2025-07-14 19:19:02 +0800

    feat(sudo): configure sudo with nix modules

--- nix/modules/system/sudo.nix
+++ nix/modules/system/sudo.nix
@@ -8,9 +8,12 @@ in
     nix.settings.trusted-users = [ "@wheel" ];
     users.users.${cfg.admin.username}.extraGroups = [ "wheel" ];
   };
-  flake.modules.darwin.default.security.pam.services.sudo_local = {
-    enable = true;
-    reattach = true;
-    touchIdAuth = true;
+  flake.modules.darwin.default.security = {
+    sudo.extraConfig = "%admin          ALL = (ALL) NOPASSWD: ALL";
+    pam.services.sudo_local = {
+      enable = true;
+      reattach = true;
+      touchIdAuth = true;
+    };
   };
 }
