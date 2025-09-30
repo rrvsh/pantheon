9247d6604e1435b1c9b1d1596b8dcee140ff226c
Mohammad Rafiq <rafiq@rrv.sh>
2025-09-30 20:54:21 +0800

    refactor(nix): move sd image to hermes

---
+++ nix/hermes.nix
@@ -0,0 +1,15 @@
+{ lib, inputs, config, ... }:
+{
+  flake.images.hermes = config.flake.nixosConfigurations.hermes.config.system.build.sdImage;
+  flake.nixosConfigurations.hermes = lib.nixosSystem {
+    modules = [
+      "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+      {
+        nixpkgs.hostPlatform.system = "aarch64-linux";
+        users.users.root.openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdsZyY3gu8IGB8MzMnLdh+ClDxQQ2RYG9rkeetIKq8n rafiq"
+        ];
+      }
+    ];
+  };
+}
