354e3cfc96a8fa6d183b97c99ecfff0d74f4821c
Mohammad Rafiq <rafiq@rrv.sh>
2025-09-30 21:32:32 +0800

    fix(nix): add system.stateVersion to hermes

--- nix/hermes.nix
+++ nix/hermes.nix
@@ -6,6 +6,7 @@
       "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
       {
         nixpkgs.hostPlatform.system = "aarch64-linux";
+        system.stateVersion = "25.11";
         users.users.root.openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdsZyY3gu8IGB8MzMnLdh+ClDxQQ2RYG9rkeetIKq8n rafiq"
         ];
